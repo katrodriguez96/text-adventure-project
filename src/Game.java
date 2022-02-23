710daedc8da4ac5f8995cec4a9c9b601fd66902a
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-23 12:40:06 -0600

    created game constructor, runs character creator each new game

--- src/Game.java
+++ src/Game.java
@@ -3,52 +3,63 @@ import java.util.Scanner;
 public class Game {
     // "Character sheet"
     public static String playerName;
-    public static int playerHP;
+    public static int playerMaxHP; // hp will never exceed this number. not bothering with temp hit points
+    public static int playerHP; // this hp will be affected by battle
     public static int playerAC;
     public static String playerClass;
     public static String playerRace;
 
-    public static void main(String[] args) {
+    // Game constructor
+    public Game () {
         Scanner sc = new Scanner(System.in);
         characterSheet();
-        System.out.println("Is this correct? [y/n]");
+        System.out.println("\nIs this correct? [y/n]"); // confirm character chosen
         String userInput = sc.nextLine();
-        if (userInput.equalsIgnoreCase("n")) {
+        if (userInput.equalsIgnoreCase("n") || userInput.equalsIgnoreCase("no")) { // runs new game to restart character creation
             playerName = null;
-            characterSheet();
+            Game newGame = new Game();
         }
-        // create a game constructor that runs character creation for each new game and ask if info is correct. if yes continue with game, if incorrect, run a new game to start over the character creator
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\t\t\t#############################");
+        System.out.println("\t\t\t# WELCOME TO THE ADVENTURE! #");
+        System.out.println("\t\t\t#############################");
+        Game newGame = new Game();
     }
 
     // fills out "character sheet" on first call, displays info on all other calls
     public static void characterSheet() {
         if (playerName == null) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Choose your adventurer:");
+            System.out.println("\nChoose your adventurer:");
             System.out.println("1. Half-Orc Barbarian 2. Halfling Rogue 3. Human Wizard");
             String userChoice = sc.nextLine();
             if (userChoice.equals("1") || userChoice.equalsIgnoreCase("half-orc") || userChoice.equalsIgnoreCase("barbarian")) {
-                playerHP = 14;
+                playerMaxHP = 14;
+                playerHP = playerMaxHP;
                 playerAC = 13;
                 playerClass = "Barbarian";
                 playerRace = "Half-Orc";
-            } else if (userChoice.equals("2") || userChoice.equalsIgnoreCase("elf") || userChoice.equalsIgnoreCase("rogue")) {
-                playerHP = 8;
+            } else if (userChoice.equals("2") || userChoice.equalsIgnoreCase("halfling") || userChoice.equalsIgnoreCase("rogue")) {
+                playerMaxHP = 8;
+                playerHP = playerMaxHP;
                 playerAC = 14;
                 playerClass = "Rogue";
                 playerRace = "Halfling";
             } else if (userChoice.equals("3") || userChoice.equalsIgnoreCase("human") || userChoice.equalsIgnoreCase("wizard")) {
-                playerHP = 8;
+                playerMaxHP = 8;
+                playerHP = playerMaxHP;
                 playerAC = 12;
                 playerClass = "Wizard";
                 playerRace = "Human";
             } else {
-                System.out.println("Invalid input, try again.");
+                System.out.println("\nInvalid input, try again.");
                 characterSheet();
             }
-            System.out.println("Enter your adventurer's name:");
+            System.out.println("\nEnter your adventurer's name:");
             playerName = sc.nextLine();
         }
-        System.out.printf("Your name is %s, a %s %s.\nYour HP is %d and your AC is %d.\n", playerName, playerRace, playerClass, playerHP, playerAC);
+        System.out.printf("\nYour name is %s, a %s %s.\nYour max HP is %d and your AC is %d.\n", playerName, playerRace, playerClass, playerHP, playerAC);
     }
 }
